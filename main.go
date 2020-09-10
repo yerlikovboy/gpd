a6a92b322349a81606aa36cca1f16c7666447c06
Ali Guler <maguler@gmail.com>
2020-09-10 21:00:36 +0000

    k8s and daemons

--- main.go
+++ main.go
@@ -7,14 +7,24 @@ import (
 	"flag"
 )
 
+func app(is_daemon bool, n_clues uint8) { 
+	for { 
+	db := couchdb.New()
+	g := db.Solution()
+	p := puzzler.Make(g, n_clues)
+	db.StorePuzzle(p) 
+	if !is_daemon  { 
+		break;
+	}
+
+	}
+
+}
 func main() {
 
 	num_clues := flag.Uint("n", 38, "number of clues (default 38)")
+	is_daemon := flag.Bool("d", false, "run as daemon")
 	flag.Parse()
-	db := couchdb.New()
-	g := db.Solution()
-	p := puzzler.Make(g, uint8(*num_clues))
-
-	db.StorePuzzle(p)
+	app(*is_daemon, uint8(*num_clues))
 
 }
